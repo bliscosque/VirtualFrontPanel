51105ac679e62fd255319939043b85c67965f236
Thiago <bliscosque@yahoo.com.br>
2022-07-31 20:40:38 -0500

    something going

---
+++ View/mainview.cpp
@@ -0,0 +1,18 @@
+#include "mainview.h"
+#include "./ui_mainview.h"
+#include "./setuptab.h"
+
+MainView::MainView(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainView)
+{
+    ui->setupUi(this);
+    auto setup_tab = new SetupTab(this);
+    ui->loSetupTab->addWidget(setup_tab);
+}
+
+MainView::~MainView()
+{
+    delete ui;
+}
+
